231f80af1187281aa2c0d19b6c4205cb3792b355
mplundgren <marcus.lundgren2@gmail.com>
2016-12-07 15:29:04 +0100

    Baby Changes
    don't worry about it... and ignore the favicon :)

--- src/client/pages/panels/ICentoCippi.js
+++ src/client/pages/panels/ICentoCippi.js
@@ -17,9 +17,8 @@ class ICentoCippi extends Component {
 
         return (
             <div className="ui form">
-                <Field placeholder="Enter value" label="Structural Characteristics" value={_.get(marker, 'bookData.structuralChar')} onChange={onChange.bind(null, 'bookData.structuralChar')} />
-                <Field placeholder="Enter value" label="Conservation" value={_.get(marker, 'bookData.conservation')} onChange={onChange.bind(null, 'bookData.conservation')} />
-
+                    <Field placeholder="Enter value" label="Structural Characteristics" value={_.get(marker, 'bookData.structuralChar')} onChange={onChange.bind(null, 'bookData.structuralChar')} />
+                    <Field placeholder="Enter value" label="Conservation" value={_.get(marker, 'bookData.conservation')} onChange={onChange.bind(null, 'bookData.conservation')} />     
                 <ImageGallery images={(marker ? marker.images : [])} typeFilter={['old-photo', 'old-map']} />
             </div>
         );
