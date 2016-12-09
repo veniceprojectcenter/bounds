7df50789f0e87ecaffc35bce2b56e60a64d337a1
Baby Wpi <baby-wpi@wpi.edu>
2016-12-09 10:24:14 +0100

    added more models

--- src/client/boundaries/index.js
+++ src/client/boundaries/index.js
@@ -1,21 +1,29 @@
-import Marghera from './Marghera';
+import Marghera from './comune/Marghera';
 import Metropolitan from './MetropolitanArea';
-import Mestre from './MestreCarpenedo';
-import Venezia from './VeneziaMuranoBurano';
+import Mestre from './comune/MestreCarpenedo';
+import Venezia from './comune/VeneziaMuranoBurano';
 import Comune from './Comune';
-import Lido from './LidoPellestrina';
-import Jesolo from './Jesolo';
-import Favaro from './FavaroVeneto';
-import Chioggia from './Chioggia';
-import Cavallino from './CavallinoTreporti';
-import Cippi from './1791';
-import Mussolini from './1924';
-import LatestConterminazione from './1990';
-import InsularBaby from './InsularBaby';
-import Codevigo from './Codevigo';
-import Chirignago from './ChirignagoZelarino';
-import CampagnaLupia from './CampagnaLupia';
-import Mira from './Mira';
+import Lido from './comune/LidoPellestrina';
+import Jesolo from './outside/Jesolo';
+import Favaro from './comune/FavaroVeneto';
+import Chioggia from './outside/Chioggia';
+import Cavallino from './outside/CavallinoTreporti';
+import Cippi from './lagoon/1791';
+import Mussolini from './lagoon/1924';
+import LatestConterminazione from './lagoon/1990';
+import InsularBaby from './models/InsularBaby';
+import GreaterVenice from './models/GreaterVenice';
+import TwoCities from './models/TwoCities';
+import Codevigo from './outside/Codevigo';
+import Chirignago from './comune/ChirignagoZelarino';
+import CampagnaLupia from './outside/CampagnaLupia';
+import Mira from './outside/Mira';
+import Pre1883 from './historic/Pre1883';
+import AdditionIn1883 from './historic/AdditionIn1883';
+import AdditionIn1917 from './historic/AdditionIn1917';
+import AdditionIn1924 from './historic/AdditionIn1924';
+import AdditionIn1926 from './historic/AdditionIn1926';
+
 
 module.exports = {
 	"Administrative Areas": {
@@ -43,7 +51,16 @@ module.exports = {
 	    "1924, Mussolini modification": Mussolini,
 	    "1990, Lagoon Boundary of Magistrato alle Acque": LatestConterminazione
 	},
+	"Historic Venice": {
+		"Pre-1883": Pre1883,
+		"Addition in 1883": AdditionIn1883,
+		"Addition in 1917": AdditionIn1917,
+		"Addition in 1924": AdditionIn1924,
+		"Addition in 1926": AdditionIn1926
+	},
 	"Models": {
-	    "Model Boundary: Insular Venice": InsularBaby
+	    "Insular Venice": InsularBaby,
+	    "Greater Venice": GreaterVenice,
+	    "Two Cities": TwoCities
 	}
 };
