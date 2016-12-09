1956621f9b5fff8e67b95165936c5099340b35e0
Baby Wpi <baby-wpi@wpi.edu>
2016-12-09 10:29:47 +0100

    fix url

--- src/client/lib/Constants.js
+++ src/client/lib/Constants.js
@@ -1,5 +1,5 @@
 module.exports = {
     AUTH_KEY: 'auth-key',
-    URL: 'http://localhost:3030/',
+    URL: 'http://bounds.herokuapp.com/',
     IMAGES_URL: 'http://bounds-imgs.s3-website-us-east-1.amazonaws.com/'
 };
